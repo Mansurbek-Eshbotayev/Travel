d93195ad982ebb5aa0b1fcdec638a68ba396dedd
Mansurbek-Eshbotayev <mustafoimron1820@gmail.com>
2022-02-19 18:55:13 +0500

    inital commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+/*
+750$ * 10900 = 8 175 000
+120 e * 12272,64 = 1 472 716,8
+8175000 + 1 472 716,8 = 9 647 716
+*/
+
+
+
+
+var a = 9647716;
+
+var b =Number(prompt(`Alisher, safar uchun qancha mablag'ingiz bor ? `,0));
+
+if(a <= b){
+  alert ("Oq yo'l sizga !")
+}else{
+  alert ("Ozgina sabr qiling !!!")
+}
